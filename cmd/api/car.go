f5b3fa7015d02e6dd529539156e3663091a5e534
aldik <aldi001122@gmail.com>
2023-02-15 13:24:19 +0600

    the database is connected and CRUD added

--- cmd/api/car.go
+++ cmd/api/car.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"armageddon/internal/models"
+	"armageddon/internal/validator"
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func (app *application) createCarHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +15,6 @@ func (app *application) createCarHandler(w http.ResponseWriter, r *http.Request)
 		Color       string `json:"color,omitempty"`
 		Year        int32  `json:"year,omitempty"`
 		Price       int32  `json:"price"`
-		IsUsed      bool   `json:"is_used"`
 	}
 
 	err := app.readJSON(w, r, &input)
@@ -23,7 +23,35 @@ func (app *application) createCarHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	car := &models.Car{
+		Brand:       input.Brand,
+		Description: input.Description,
+		Color:       input.Color,
+		Year:        input.Year,
+		Price:       input.Price,
+	}
+
+	v := validator.New()
+
+	if models.ValidateCar(v, car); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.models.Car.Insert(car)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/car/%d", car.ID))
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"car": car}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
 }
 
 func (app *application) showCarHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,18 +61,117 @@ func (app *application) showCarHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	car := models.Car{
-		ID:          id,
-		CreatedAt:   time.Now(),
-		Brand:       "Mercedes?",
-		Description: "Null?",
-		Color:       "Null?",
-		Year:        0,
-		Price:       0,
-		IsUsed:      false,
+	car, err := app.models.Car.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"car": car}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) updateCarHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	car, err := app.models.Car.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	var input struct {
+		Brand       *string `json:"brand"`
+		Description *string `json:"description"`
+		Color       *string `json:"color"`
+		Year        *int32  `json:"year"`
+		Price       *int32  `json:"price"`
+		IsUsed      *bool   `json:"is_used"`
+	}
+
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if input.Brand != nil {
+		car.Brand = *input.Brand
+	}
+
+	if input.Description != nil {
+		car.Description = *input.Description
+	}
+
+	if input.Color != nil {
+		car.Color = *input.Color
+	}
+
+	if input.Year != nil {
+		car.Year = *input.Year
+	}
+
+	if input.Price != nil {
+		car.Price = *input.Price
+	}
+
+	if input.IsUsed != nil {
+		car.IsUsed = *input.IsUsed
+	}
+
+	v := validator.New()
+	if models.ValidateCar(v, car); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.models.Car.Update(car)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"car": car}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) deleteCarHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	err = app.models.Car.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "car successfully deleted"}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
--- internal/models/car.go
+++ internal/models/car.go
@@ -1,6 +1,16 @@
 package models
 
-import "time"
+import (
+	"armageddon/internal/validator"
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+)
+
+type CarModel struct {
+	DB *sql.DB
+}
 
 type Car struct {
 	ID          int64     `json:"id"`
@@ -11,4 +21,117 @@ type Car struct {
 	Year        int32     `json:"year,omitempty"`
 	Price       int32     `json:"price"`
 	IsUsed      bool      `json:"is_used"`
+}
+
+func (m CarModel) Insert(car *Car) error {
+	query := `
+		INSERT INTO car (brand, description, color, year, price)
+		VALUES ($1, $2, $3, $4, $5)
+		RETURNING id, created_at, is_used`
+
+	args := []any{car.Brand, car.Description, car.Color, car.Year, car.Price}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&car.ID, &car.CreatedAt, &car.IsUsed)
+}
+
+func (m CarModel) Get(id int64) (*Car, error) {
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query := `SELECT * FROM car WHERE id = $1`
+
+	var car Car
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
+		&car.ID,
+		&car.CreatedAt,
+		&car.Brand,
+		&car.Description,
+		&car.Color,
+		&car.Year,
+		&car.Price,
+		&car.IsUsed,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &car, nil
+}
+
+func (m CarModel) Update(car *Car) error {
+	query := `
+		UPDATE car
+		SET brand = $1, description = $2, color = $3, year = $4, price = $5, is_used = $6
+		WHERE id = $7
+		RETURNING created_at`
+
+	args := []any{
+		car.Brand,
+		car.Description,
+		car.Color,
+		car.Year,
+		car.Price,
+		car.IsUsed,
+		car.ID,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&car.CreatedAt)
+}
+
+func (m CarModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `DELETE FROM car WHERE id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	result, err := m.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
+	return nil
+}
+
+func ValidateCar(v *validator.Validator, car *Car) {
+	v.Check(car.Brand != "", "brand", "must be provided")
+	v.Check(len(car.Brand) <= 500, "brand", "must not be more than 500 bytes long")
+
+	v.Check(car.Description != "", "brand", "must be provided")
+	v.Check(car.Color != "", "color", "must be provided")
+
+	v.Check(car.Year != 0, "year", "must be provided")
+	v.Check(car.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(car.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(car.Price != 0, "price", "must be provided")
+	v.Check(car.Price > 0, "price", "must be a positive integer")
 }
